1ad40943bf2c1f34640501fed3864b11da74ee02
Johnmx3a65 <ivanstojanoff2015@gmail.com>
2020-11-28 16:55:34 +0200

    добавлен logout

--- src/main/java/sap/webapp/controlller/UserController.java
+++ src/main/java/sap/webapp/controlller/UserController.java
@@ -1,16 +1,24 @@
 package sap.webapp.controlller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import sap.webapp.bindingModel.UserBindingModel;
 import sap.webapp.entity.Role;
 import sap.webapp.entity.User;
 import sap.webapp.repository.RoleRepository;
 import sap.webapp.repository.UserRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class UserController {
@@ -53,5 +61,16 @@ public class UserController {
         model.addAttribute("view", "user/login");
 
         return "base-layout";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if(auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login?logout";
     }
 }
