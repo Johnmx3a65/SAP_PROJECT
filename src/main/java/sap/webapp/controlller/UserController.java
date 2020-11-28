b4975f3c059c5bbbafe3470b8dc17cc27e80d5de
Johnmx3a65 <ivanstojanoff2015@gmail.com>
2020-11-28 16:42:27 +0200

    завершена регистрация и добавлено окно логина

--- src/main/java/sap/webapp/controlller/UserController.java
+++ src/main/java/sap/webapp/controlller/UserController.java
@@ -1,9 +1,14 @@
 package sap.webapp.controlller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sap.webapp.bindingModel.UserBindingModel;
+import sap.webapp.entity.Role;
+import sap.webapp.entity.User;
 import sap.webapp.repository.RoleRepository;
 import sap.webapp.repository.UserRepository;
 
@@ -18,6 +23,34 @@ public class UserController {
     @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("view", "user/register");
+
+        return "base-layout";
+    }
+
+    @PostMapping("/register")
+    public String registerProcess(UserBindingModel userBindingModel){
+        if(!userBindingModel.getPassword().equals(userBindingModel.getConfirmPassword())){
+            return "redirect:/register";
+        }
+
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+        User user = new User(userBindingModel.getEmail(),
+                             userBindingModel.getFullName(),
+                             bCryptPasswordEncoder.encode(userBindingModel.getPassword()));
+
+        Role userRole = this.roleRepository.findByName("ROLE_USER");
+
+        user.addRole(userRole);
+
+        this.userRepository.saveAndFlush(user);
+
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("view", "user/login");
 
         return "base-layout";
     }
