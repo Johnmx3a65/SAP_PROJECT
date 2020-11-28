ccbe31cfe91bb9ee2e6eb162668bb8053fc21b13
Johnmx3a65 <ivanstojanoff2015@gmail.com>
2020-11-28 16:02:15 +0200

    добавлен контроллер для начальной страницы

---
+++ src/main/java/sap/webapp/controlller/HomeController.java
@@ -0,0 +1,15 @@
+package sap.webapp.controlller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String index(Model model){
+        model.addAttribute("view", "home/index");
+        return "base-layout";
+    }
+}
