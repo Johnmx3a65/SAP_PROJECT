5e2a61db0b2d2ba20ed429306205a6c91922f4d4
Johnmx3a65 <ivanstojanoff2015@gmail.com>
2020-12-04 18:49:45 +0200

    редактирование потребителя

--- src/main/java/sap/webapp/controlller/admin/AdminUserController.java
+++ src/main/java/sap/webapp/controlller/admin/AdminUserController.java
@@ -1,16 +1,24 @@
 package sap.webapp.controlller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.util.StringUtils;
+import sap.webapp.bindingModel.UserEditBindingModel;
+import sap.webapp.entity.Role;
 import sap.webapp.entity.User;
 import sap.webapp.repository.ProductRepository;
 import sap.webapp.repository.RoleRepository;
 import sap.webapp.repository.UserRepository;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/admin/users")
@@ -18,7 +26,7 @@ public class AdminUserController {
     @Autowired
     private UserRepository userRepository;
     @Autowired
-    private ProductRepository productRepository;
+    private RoleRepository roleRepository;
     @Autowired
     private RoleRepository repository;
 
@@ -30,5 +38,51 @@ public class AdminUserController {
         model.addAttribute("view", "admin/user/list");
 
         return "base-layout";
+    }
+
+    @GetMapping("edit/{id}")
+    public String edit(Model model, @PathVariable Integer id){
+        if(!this.userRepository.existsById(id)){
+            return "redirect:/admin/users";
+        }
+
+        User user = this.userRepository.getOne(id);
+        List<Role> roles = this.roleRepository.findAll();
+
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+        model.addAttribute("view", "admin/user/edit");
+
+        return "base-layout";
+    }
+
+    @PostMapping("edit/{id}")
+    public String editProcess(UserEditBindingModel userBindingModel, @PathVariable Integer id){
+        if(!this.userRepository.existsById(id)){
+            return "redirect:/admin/users";
+        }
+        User user = this.userRepository.getOne(id);
+
+        if(!StringUtils.isEmpty(userBindingModel.getPassword()) && !StringUtils.isEmpty(userBindingModel.getConfirmPassword())){
+            if(userBindingModel.getPassword().equals(userBindingModel.getConfirmPassword())){
+                BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+                user.setPassword(bCryptPasswordEncoder.encode(userBindingModel.getPassword()));
+            }
+        }
+        user.setFullName(userBindingModel.getFullName());
+        user.setEmail(userBindingModel.getEmail());
+
+        Set<Role> roles = new HashSet<>();
+
+        for (Integer roleId : userBindingModel.getRoles()){
+            roles.add(this.repository.getOne(roleId));
+        }
+
+        user.setRoles(roles);
+
+        this.userRepository.saveAndFlush(user);
+
+        return "redirect:/admin/users/";
     }
 }
